05cf62ad6eda5c33ba3609fdcced366241d513c0
jujuxiaotian <3190302384@qq.com>
2019-12-17 09:29:52 +0800

    测试添加bat程序

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,26 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public static void Main(string[] args)
+        {
+            Process proc = null;
+            try
+            {
+                string targetDir = string.Format(@"D:\My_OwnData\jujuxiaotian\CurrentLearning\ClassLibrary1\ClassLibrary1\");//this is where mybatch.bat lies
+                proc = new Process();
+                proc.StartInfo.WorkingDirectory = targetDir;
+                proc.StartInfo.FileName = "ExitCopyKzjhAndOpen.bat";
+                proc.StartInfo.Arguments = string.Format("10");//this is argument
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+                proc.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred :{0},{1}", ex.Message, ex.StackTrace.ToString());
+            }
+            return;
+        }
     }
+    
 }
---
+++ ClassLibrary1/ClassLibrary1/ExitCopyKzjhAndOpen.bat
@@ -0,0 +1,16 @@
+//:start
+@echo off
+REM 声明采用UTF-8编码
+chcp 65001
+REM echo test
+REM echo 中文测试
+REM pause
+
+taskkill /f /im "GSPMain.exe"
+
+copy D:\My_JCData\Source\JD预算管理系统\Version_ZB\JC.YS.Kzjh\JC.YS.Ctl.Kzjh\bin\Debug\JC.YS.Ctl.Kzjh.dll D:\My_JD\总部测试_84\JC\JDCW\
+pause
+
+
+start D:\My_JD\总部测试_84\JC\JDCW\GSFramework.exe
+//goto start
